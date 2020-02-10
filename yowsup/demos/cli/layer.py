38c4af1124b0a5132d338c6862be31eaea71a2f0
KriwoschejewAnton <akrivoshey@wiley.com>
2020-02-10 19:55:09 +0300

    output disabled

--- yowsup/demos/cli/layer.py
+++ yowsup/demos/cli/layer.py
@@ -101,7 +101,6 @@ class YowsupCliLayer(YowInterfaceLayer):
         if self.connected:
             return True
         else:
-            self.output("Not connected", tag = "Error", prompt = False)
             return False
 
     ########## PRESENCE ###############
@@ -131,7 +130,7 @@ class YowsupCliLayer(YowInterfaceLayer):
 
     def L(self):
         if self.connected:
-            return self.output("Already connected, disconnect first")
+            pass
         threading.Thread(target=lambda: self.getLayerInterface(YowNetworkLayer).connect()).start()
         return True
 
@@ -148,7 +147,6 @@ class YowsupCliLayer(YowInterfaceLayer):
             lastseen = time.time()
         ##
         message = "%s %s %s" % (entity.getFrom(), status, lastseen)
-        self.output(message)
         self.message_send(recipient, message )
         
     @ProtocolEntityCallback("chatstate")
@@ -160,22 +158,10 @@ class YowsupCliLayer(YowInterfaceLayer):
         if not isinstance(entity, ResultStatusesIqProtocolEntity):  # already printed somewhere else
             print(entity)
 
-    @ProtocolEntityCallback("receipt")
-    def onReceipt(self, entity):
-        self.toLower(entity.ack())
-
-    @ProtocolEntityCallback("ack")
-    def onAck(self, entity):
-        #formattedDate = datetime.datetime.fromtimestamp(self.sentCache[entity.getId()][0]).strftime('%d-%m-%Y %H:%M')
-        #print("%s [%s]:%s"%(self.username, formattedDate, self.sentCache[entity.getId()][1]))
-        if entity.getClass() == "message":
-            self.output(entity.getId(), tag = "Sent")
-            #self.notifyInputThread()
 
     @ProtocolEntityCallback("success")
     def onSuccess(self, entity):
         self.connected = True
-        self.output("Logged in!", "Auth", prompt = False)
         self.notifyInputThread()
         self.presence_available()
         for ph in phones:
@@ -185,66 +171,6 @@ class YowsupCliLayer(YowInterfaceLayer):
     @ProtocolEntityCallback("failure")
     def onFailure(self, entity):
         self.connected = False
-        #self.output("Login Failed, reason: %s" % entity.getReason(), prompt = False)
-
-    @ProtocolEntityCallback("notification")
-    def onNotification(self, notification):
-        notificationData = notification.__str__()
-        if notificationData:
-            self.output(notificationData, tag = "Notification")
-        else:
-            self.output("From :%s, Type: %s" % (self.jidToAlias(notification.getFrom()), notification.getType()), tag = "Notification")
-
-    @ProtocolEntityCallback("message")
-    def onMessage(self, message):
-        messageOut = ""
-        if message.getType() == "text":
-            #self.output(message.getBody(), tag = "%s [%s]"%(message.getFrom(), formattedDate))
-            messageOut = self.getTextMessageBody(message)
-        elif message.getType() == "media":
-            messageOut = self.getMediaMessageBody(message)
-        else:
-            messageOut = "Unknown message type %s " % message.getType()
-            print(messageOut.toProtocolTreeNode())
-
-        formattedDate = datetime.datetime.fromtimestamp(message.getTimestamp()).strftime('%d-%m-%Y %H:%M')
-        sender = message.getFrom() if not message.isGroupMessage() else "%s/%s" % (message.getParticipant(False), message.getFrom())
-
-        output = self.__class__.MESSAGE_FORMAT % (sender, formattedDate, message.getId(), messageOut)
-
-        self.output(output, tag = None, prompt = not self.sendReceipts)
-        if self.sendReceipts:
-            self.toLower(message.ack(self.sendRead))
-            self.output("Sent delivered receipt"+" and Read" if self.sendRead else "", tag = "Message %s" % message.getId())
-
-    def getTextMessageBody(self, message):
-        if isinstance(message, TextMessageProtocolEntity):
-            return message.conversation
-        elif isinstance(message, ExtendedTextMessageProtocolEntity):
-            return str(message.message_attributes.extended_text)
-        else:
-            raise NotImplementedError()
-
-    def getMediaMessageBody(self, message):
-        # type: (DownloadableMediaMessageProtocolEntity) -> str
-        return str(message.message_attributes)
-
-    def getDownloadableMediaMessageBody(self, message):
-        return "[media_type={media_type}, length={media_size}, url={media_url}, key={media_key}]".format(
-            media_type=message.media_type,
-            media_size=message.file_length,
-            media_url=message.url,
-            media_key=base64.b64encode(message.media_key)
-        )
-
-    def doSendMedia(self, mediaType, filePath, url, to, ip = None, caption = None):
-        if mediaType == RequestUploadIqProtocolEntity.MEDIA_TYPE_IMAGE:
-        	entity = ImageDownloadableMediaMessageProtocolEntity.fromFilePath(filePath, url, ip, to, caption = caption)
-        elif mediaType == RequestUploadIqProtocolEntity.MEDIA_TYPE_AUDIO:
-        	entity = AudioDownloadableMediaMessageProtocolEntity.fromFilePath(filePath, url, ip, to)
-        elif mediaType == RequestUploadIqProtocolEntity.MEDIA_TYPE_VIDEO:
-        	entity = VideoDownloadableMediaMessageProtocolEntity.fromFilePath(filePath, url, ip, to, caption = caption)
-        self.toLower(entity)
 
     def __str__(self):
         return "CLI Interface Layer"
